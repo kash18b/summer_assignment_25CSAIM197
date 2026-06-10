08fe8c8ad79a12a01c8fc1447e5ee456c416839e
kash18b <kashishbiswakarma2@gmail.com>
2026-06-10 13:13:22 +0530

    Update program1.c

--- Day-3/program1.c
+++ Day-3/program1.c
@@ -1,1 +1,28 @@
-.se
+#include <stdio.h>
+
+int main() {
+    int num, i, flag = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        printf("Not Prime\n");
+        return 0;
+    }
+
+    for (i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0) {
+        printf("Prime\n");
+    } else {
+        printf("Not Prime\n");
+    }
+
+    return 0;
+}
