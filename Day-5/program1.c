dce6e09b044bf6480cb31bb23b997fdf4b58ca68
kash18b <kashishbiswakarma2@gmail.com>
2026-06-10 13:20:40 +0530

    Update program1.c

--- Day-5/program1.c
+++ Day-5/program1.c
@@ -1,1 +1,27 @@
-.se
+#include <stdio.h>
+
+int main() {
+    int num, i, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        printf("Not a Perfect Number\n");
+        return 0;
+    }
+
+    for (i = 1; i <= num / 2; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == num) {
+        printf("Perfect Number\n");
+    } else {
+        printf("Not a Perfect Number\n");
+    }
+
+    return 0;
+}
