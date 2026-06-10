08bd3cfddda71ede619d5648ae7f9a5a77bdfa5a
kash18b <kashishbiswakarma2@gmail.com>
2026-06-10 13:27:40 +0530

    Update program1.c

--- Day-7/program1.c
+++ Day-7/program1.c
@@ -1,1 +1,24 @@
-.
+#include <stdio.h>
+
+long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Factorial not defined\n");
+        return 0;
+    }
+
+    printf("Factorial = %lld\n", factorial(n));
+
+    return 0;
+}
