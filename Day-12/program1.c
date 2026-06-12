c240589cdfc1b0914aa9530339dcfc30819b9481
kash18b <kashishbiswakarma2@gmail.com>
2026-06-12 12:06:19 +0530

    Update program1.c

--- Day-12/program1.c
+++ Day-12/program1.c
@@ -1,1 +1,29 @@
-.
+#include <stdio.h>
+
+int isPalindrome(int n) {
+    int original = n;
+    int reverse = 0;
+    int digit;
+
+    while (n > 0) {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n = n / 10;
+    }
+
+    return original == reverse;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isPalindrome(num))
+        printf("Palindrome Number\n");
+    else
+        printf("Not a Palindrome Number\n");
+
+    return 0;
+}
