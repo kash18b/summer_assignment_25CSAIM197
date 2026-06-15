66e3e5334c7213eb4bb173248d2b85fa9c592e26
kash18b <kashishbiswakarma2@gmail.com>
2026-06-15 16:00:07 +0530

    Update program1.c

--- Day-15/program1.c
+++ Day-15/program1.c
@@ -1,1 +1,28 @@
-.
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, temp;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    printf("Reversed array:\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
